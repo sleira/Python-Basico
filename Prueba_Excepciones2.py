aa1fc10e384f3a01537a1d022ac8db7aa5b74d31
Sharon Leira Rojas <84730310+sleira@users.noreply.github.com>
2021-05-30 15:21:51 +0200

    Add files via upload

---
+++ Prueba_Excepciones2.py
@@ -0,0 +1,16 @@
+def divide():
+	try:
+		op1=(float(input("Introduce el primer numero: ")))
+		op2=(float(input("Introduce el segundo numero: ")))
+		print("La division es: " + str(op1/op2))
+
+	except ValueError:
+		print("El valor intoducido es erroneo")
+
+	except ZeroDivisionError:
+		print("No se puede dividir entre 0")
+
+	finally:
+		print("Calculo finalizado")
+
+divide()
